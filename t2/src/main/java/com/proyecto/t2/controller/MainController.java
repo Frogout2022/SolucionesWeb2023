c5ad4b95545d0e159ce41ebc7d2a935f25cdb4c6
Milhos <milhos.sihuay@gmail.com>
2023-05-05 02:01:26 -0500

    Plantilla funcional
    Prueba de plantilla

---
+++ t2/src/main/java/com/proyecto/t2/controller/MainController.java
@@ -0,0 +1,16 @@
+package com.proyecto.t2.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+
+@RequestMapping("/Proyecto")
+public class MainController {
+    @RequestMapping("/")
+    public String inicio(){
+        return "index";
+    }
+
+    
+}
---
+++ t2/src/main/resources/static/js/libreria.js
@@ -0,0 +1,6 @@
+function enviarDatos(){
+
+}
+function recibirDatos(){
+
+}
