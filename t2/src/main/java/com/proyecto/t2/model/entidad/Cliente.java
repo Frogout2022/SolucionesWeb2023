677206ab9e21c5426512e09365542e06dc9f9f7e
Milhos <milhos.sihuay@gmail.com>
2023-06-03 05:34:17 -0500

    create clase Cliente

---
+++ t2/src/main/java/com/proyecto/t2/model/entidad/Cliente.java
@@ -0,0 +1,97 @@
+package com.proyecto.t2.model.entidad;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+
+@Entity
+@Table(name="cliente")
+public class Cliente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="idcliente")
+    private Long id;
+
+    @Column(name="nombre")
+    private String nombre;
+
+    @Column(name="direccion")
+    private String direccion;
+
+    @Column(name="telefono")
+    private String telefono;
+
+    @Column(name="correo")
+    private String correo;
+
+    @Column(name="clave")
+    private String clave;
+
+    @Column(name="distrito")
+    private String distrito;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getClave() {
+        return clave;
+    }
+
+    public void setClave(String clave) {
+        this.clave = clave;
+    }
+
+    public String getDistrito() {
+        return distrito;
+    }
+
+    public void setDistrito(String distrito) {
+        this.distrito = distrito;
+    }
+
+    
+
+    
+}
