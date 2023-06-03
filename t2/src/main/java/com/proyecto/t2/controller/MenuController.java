b1279128d609d7f56c390f5fe370d787a58938f2
AlexDrax03 <1cizquierdochristian@gmail.com>
2023-06-03 07:47:22 -0500

    Añadir, Modificar, Ver y eliminar Menu

--- t2/src/main/java/com/proyecto/t2/controller/MenuController.java
+++ t2/src/main/java/com/proyecto/t2/controller/MenuController.java
@@ -1,13 +1,47 @@
 package com.proyecto.t2.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.proyecto.t2.model.entidad.Menu;
+import com.proyecto.t2.model.service.IMenuService;
 
 @Controller
 @RequestMapping("/menu")
 public class MenuController {
+
+    @Autowired
+    private IMenuService menuService;
     @RequestMapping("/")
-    public String inicio(){
+    public String inicio(Model model){
+        Menu menu=new Menu();
+        model.addAttribute("menu", menu);
+        model.addAttribute("titulo", "Registrar nuevo menu");
+        model.addAttribute("listaMenu", menuService.mostrarMenus());
         return "menu/inicio";
+    }
+
+    @RequestMapping("/guardar")
+    public String guardar(Menu menu){
+        menuService.guardarMenu(menu);
+        return "redirect:/menu/";
+    }
+
+    @RequestMapping("/mostrarEditar/{id}")
+    public String editar(@PathVariable(value = "id") Long id, Model model){
+        Menu Menu = new Menu();
+        Menu = menuService.buscarMenu(id);
+        model.addAttribute("menu", Menu);
+        model.addAttribute("titulo", "Modificar menu");
+        model.addAttribute("listaMenu", menuService.mostrarMenus());
+        return "menu/inicio"; 
+    }
+    @RequestMapping("/eliminar/{id}")
+    public String eliminar(@PathVariable(value = "id") Long id){
+        menuService.eliminarMenu(id);
+        return "redirect:/menu/";
     }
 }
--- t2/src/main/java/com/proyecto/t2/model/service/MenuServiceImp.java
+++ t2/src/main/java/com/proyecto/t2/model/service/MenuServiceImp.java
@@ -1,5 +1,8 @@
 package com.proyecto.t2.model.service;
 
+
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,8 +14,24 @@ public class MenuServiceImp implements IMenuService{
 
     @Autowired
     private IMenuDAO menuDAO;
+
     @Override
     public void guardarMenu(Menu menu) {
         menuDAO.save(menu);
+    }
+
+    @Override
+    public List<Menu> mostrarMenus(){
+        return (List<Menu>)menuDAO.findAll();
+    }
+
+    @Override
+    public Menu buscarMenu(Long id){
+        return menuDAO.findById(id).orElse(null);
+    }
+
+    @Override
+    public void eliminarMenu(Long id) {
+        menuDAO.deleteById(id);
     }
 }
