d64f55c8ab309caa1d9e95f2321646685362e0e0
Milhos <milhos.sihuay@gmail.com>
2023-06-03 04:25:41 -0500

    Create polleria.sql

---
+++ t2/src/main/resources/sql/polleria.sql
@@ -0,0 +1,102 @@
+CREATE DATABASE IF NOT EXISTS Polleria;
+
+USE Polleria;
+
+CREATE TABLE IF NOT EXISTS Cliente (
+    IDCliente INT AUTO_INCREMENT NOT NULL,
+    Nombre VARCHAR(50) NULL,
+    Direccion VARCHAR(50) NULL,
+    Telefono VARCHAR(13) NULL,
+    Correo VARCHAR(20) NOT NULL,
+    Clave VARCHAR(15) NOT NULL,
+    Distrito VARCHAR(50) NULL,
+    PRIMARY KEY (IDCliente)
+) ;
+
+CREATE TABLE IF NOT EXISTS Bebida (
+    IDBebida INT AUTO_INCREMENT NOT NULL,
+    Nombre VARCHAR(20) NULL,
+    Tipo VARCHAR(10) NULL,
+    Tamano VARCHAR(10) NULL,
+    Precio DECIMAL(5, 2) NULL,
+    Stock INT NULL,
+    PRIMARY KEY (IDBebida)
+) ;
+
+CREATE TABLE IF NOT EXISTS Menu (
+    IDMenu INT AUTO_INCREMENT NOT NULL,
+    Nombre VARCHAR(20) NULL,
+    Tipo VARCHAR(10) NULL,
+    Precio DECIMAL(5, 2) NULL,
+    Stock INT NULL,
+    PRIMARY KEY (IDMenu)
+);
+
+CREATE TABLE IF NOT EXISTS Piqueo (
+    IDPiqueo INT AUTO_INCREMENT NOT NULL,
+    Nombre VARCHAR(20) NULL,
+    Tipo VARCHAR(10) NULL,
+    Precio DECIMAL(5, 2) NULL,
+    Porcion VARCHAR(10) NULL,
+    Stock INT NULL,
+    PRIMARY KEY (IDPiqueo)
+) ;
+
+CREATE TABLE IF NOT EXISTS Pollo (
+    IDPollo INT AUTO_INCREMENT NOT NULL,
+    Nombre VARCHAR(20) NULL,
+    Precio DECIMAL(5, 2) NULL,
+    Stock INT NULL,
+    Cantidad INT NULL,
+    PRIMARY KEY (IDPollo)
+);
+
+
+CREATE TABLE IF NOT EXISTS Combo (
+    IDCombo INT AUTO_INCREMENT NOT NULL,
+    Nombre VARCHAR(20) NULL,
+    Precio DECIMAL(5, 2) NULL,
+    Stock INT NULL,
+    IDBebida INT NULL,
+    IDMenu INT NULL,
+    IDPollo INT NULL,
+    IDPiqueo INT NULL,
+    PRIMARY KEY (IDCombo),
+    FOREIGN KEY (IDBebida) REFERENCES Bebida (IDBebida),
+    FOREIGN KEY (IDMenu) REFERENCES Menu (IDMenu),
+    FOREIGN KEY (IDPiqueo) REFERENCES Piqueo (IDPiqueo),
+    FOREIGN KEY (IDPollo) REFERENCES Pollo (IDPollo)
+) ;
+
+CREATE TABLE IF NOT EXISTS Compra (
+    IDCompra INT AUTO_INCREMENT NOT NULL,
+    Cantidad INT NULL,
+    FechaCompra DATETIME NULL,
+    TotalPago DECIMAL(6, 2) NULL,
+    IDBebida INT NULL,
+    IDMenu INT NULL,
+    IDCombo INT NULL,
+    IDPollo INT NULL,
+    IDCliente INT NULL,
+    IDPiqueo INT NULL,
+    PRIMARY KEY (IDCompra),
+    FOREIGN KEY (IDBebida) REFERENCES Bebida (IDBebida),
+    FOREIGN KEY (IDMenu) REFERENCES Menu (IDMenu),
+    FOREIGN KEY (IDCombo) REFERENCES Combo (IDCombo),
+    FOREIGN KEY (IDPollo) REFERENCES Pollo (IDPollo),
+    FOREIGN KEY (IDCliente) REFERENCES Cliente (IDCliente),
+    FOREIGN KEY (IDPiqueo) REFERENCES Piqueo (IDPiqueo)
+);
+
+
+CREATE TABLE IF NOT EXISTS Trabajador (
+    IDTrabajador INT AUTO_INCREMENT NOT NULL,
+    Nombre VARCHAR(50) NULL,
+    Direccion VARCHAR(50) NULL,
+    Telefono VARCHAR(13) NULL,
+    Usuario VARCHAR(20) NOT NULL,
+    Clave VARCHAR(15) NOT NULL,
+    PRIMARY KEY (IDTrabajador)
+);
+
+
