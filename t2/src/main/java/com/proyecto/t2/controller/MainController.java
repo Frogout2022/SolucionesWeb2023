1836ebf7d1ba17dc6032aedd838530636a3c15e9
Milhos <milhos.sihuay@gmail.com>
2023-06-22 13:35:51 -0500

    Separación v1.3
    CLIENTE -> EXTRANET
    admin -> INTRANET

--- t2/src/main/java/com/proyecto/t2/controller/MainController.java
+++ t2/src/main/java/com/proyecto/t2/controller/MainController.java
@@ -39,23 +39,18 @@ public class MainController {
     @RequestMapping("/intranet")
     public String dash(Model model){
         if(User.sesion) {
-            if(User.login_emp){
-                //String nom = User.user.getNombre();
-                //model.addAttribute("nombre", nom);
-                return "empleado/intranet";
-            }
-            if(User.login_cli){
-                return "cliente/extranetTest"; //redirect -> ruta de GetMapping(link) - no redirect -> ubicacion html
-            }
-          
+            if(User.login_emp) return "empleado/intranet";
+            if(User.login_cli) return "cliente/extranetTest"; //redirect -> ruta de GetMapping(link) - no redirect -> ubicacion html
         }else return "redirect:/login";
-
         return "";
     }
 
     @GetMapping("/extranet")
     public String extra(Model model){
         if(User.sesion && User.login_cli){
+                String nom_completo = User.user.getNombre();
+                String[] primer_nom = nom_completo.trim().split("\\s+");
+                model.addAttribute("nombre", primer_nom[0]);
                 return "cliente/extranetTest";
         }else if(User.sesion && User.login_emp){
                 return "redirect:/intranet";
