5ced203a6b629d879479f10c7e60e7e0099914f1
Milhos <milhos.sihuay@gmail.com>
2023-06-24 11:41:13 -0500

    update ComboController
    mensaje flash al editar, borrar y guardar

--- t2/src/main/java/com/proyecto/t2/controller/sistema/ComboController.java
+++ t2/src/main/java/com/proyecto/t2/controller/sistema/ComboController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.proyecto.t2.model.service.sistema.IPolloService;
 import com.proyecto.t2.model.entidad.Combo;
@@ -35,7 +36,7 @@ public class ComboController {
     private IPolloService iPolloService;
 
     @RequestMapping("/")
-    public String inicio(Combo combo, Model model){
+    public String inicio(Combo combo, Model model,RedirectAttributes flash){
         //LISTAR EN COMBOBOX DEL FORM
         model.addAttribute("listaBebidas", bebidaService.mostrBebidas());
         model.addAttribute("listaMenu", iMenuService.mostrarMenus());
@@ -45,35 +46,40 @@ public class ComboController {
        
         //LISTAR COMBOS REGISTRADOS EN LA TABLA INFERIOR
         model.addAttribute("listaCombo", comboService.mostrarCombos());
+
+       
         return "sistema/combo";
     }
 
     @PostMapping("/guardar")
-    public String guardar(Combo combo) {
+    public String guardar(Combo combo,RedirectAttributes flash) {
         if(combo.getBebida().getId()== null) combo.setBebida(null);
         if(combo.getMenu().getId()== null) combo.setMenu(null);
         if(combo.getPiqueo().getId()== null) combo.setPiqueo(null);
         if(combo.getPollo().getId()== null) combo.setPollo(null);
-
+        String rpt = "Se añádio el combo: "+combo.getNombre();
+        flash.addFlashAttribute("mensaje", rpt);
         
+        comboService.guardarCombos(combo);
+        return "redirect:/intranet/combo/";
+    }
+    @PostMapping("/editar")
+    public String editar(Combo combo,RedirectAttributes flash) {
+        if(combo.getBebida().getId()== null) combo.setBebida(null);
+        if(combo.getMenu().getId()== null) combo.setMenu(null);
+        if(combo.getPiqueo().getId()== null) combo.setPiqueo(null);
+        if(combo.getPollo().getId()== null) combo.setPollo(null);
+        String rpt = "Se actualizó el combo: "+combo.getNombre();
+        flash.addFlashAttribute("mensaje", rpt);
         
         comboService.guardarCombos(combo);
         return "redirect:/intranet/combo/";
     }
 
-    @RequestMapping("/mostrarEditar/{id}")
-    public String editar(@PathVariable(value = "id") Long id, Model model){
-        Combo combo = new Combo();
-        
-        combo = comboService.buscarCombo(id);
-        model.addAttribute("combo", combo);
-        model.addAttribute("titulo", "Modificar combo");
-        model.addAttribute("listaCombos", comboService.mostrarCombos());
-        return "sistema/combo"; 
-    }
-
     @RequestMapping("/eliminar/{id}")
-    public String eliminar(@PathVariable(value = "id") Long id){
+    public String eliminar(@PathVariable(value = "id") Long id, RedirectAttributes flash ){
+         String rpt = "Se eliminó el combo con id: "+id;
+        flash.addFlashAttribute("mensaje", rpt);
         comboService.eliminarCombo(id);
         return "redirect:/intranet/combo/";
     }
