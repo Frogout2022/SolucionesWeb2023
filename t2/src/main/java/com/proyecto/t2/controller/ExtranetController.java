8722926ba19771a5c6c876a131da7d2fd00a4a61
Pprodigy <42827132+Pprodigy@users.noreply.github.com>
2023-06-30 16:17:35 -0500

    Enlace de las páginas de la extranet

--- t2/src/main/java/com/proyecto/t2/controller/ExtranetController.java
+++ t2/src/main/java/com/proyecto/t2/controller/ExtranetController.java
@@ -9,26 +9,30 @@ public class ExtranetController {
     
     @RequestMapping("/")
     public String inicio_extranet(){
-        return "extranet";
+        return "cliente/extranet";
     }
     @RequestMapping("/carrito")
     public String extranet_carrito(){
-        return "extranet/extranet_carrito";
+        return "cliente/extranet_carrito";
     }
     @RequestMapping("/historial")
     public String extranet_historial(){
-        return "extranet/extranet_historial";
+        return "cliente/extranet_historial";
     }
     @RequestMapping("/notificaciones")
     public String extranet_notificaciones(){
-        return "extranet/extranet_notificaciones";
+        return "cliente/extranet_notificaciones";
     }
     @RequestMapping("/reclamos")
     public String extranet_reclamos(){
-        return "extranet/extranet_reclamos";
+        return "cliente/extranet_reclamos";
     }
     @RequestMapping("/actualizar")
     public String extranet_actu_datos(){
-        return "extranet/extranet_actu_datos";
+        return "cliente/extranet_actu_datos";
+    }
+    @RequestMapping("/boletas")
+    public String extranet_boleta(){
+        return "cliente/extranet_boleta";
     }
 }
