00479f9426227e60f7662792bd11900efdd575fe
Milhos <milhos.sihuay@gmail.com>
2023-07-03 00:41:59 -0500

    Update IbebidaDAO.java

--- t2/src/main/java/com/proyecto/t2/model/dao/IbebidaDAO.java
+++ t2/src/main/java/com/proyecto/t2/model/dao/IbebidaDAO.java
@@ -5,5 +5,5 @@ import org.springframework.data.repository.CrudRepository;
 import com.proyecto.t2.model.entidad.Bebida;
 
 public interface IbebidaDAO extends CrudRepository<Bebida,Long>{
-    
+    // hola
 }
